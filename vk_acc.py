d1a6ba9cfca51925581ee14530614e2a4a3ae98f
vlzhr <izhur27@gmail.com>
2017-05-31 22:12:41 +0300

    Добавление токена vk для удобства тестирования

--- vk_acc.py
+++ vk_acc.py
@@ -13,7 +13,7 @@ vlzhr = vk_api.VkApi(token = vlzhr_token)
 token = u''
 vk = vk_api.VkApi(token = token)
 
-vlzhr_token = u''
+vlzhr_token = u'52a178721e01cf2dc1ae322988c77239b26dc16351604b5778bcc42f45ef7ab7f6f11e8c7006a14faa7b7'  # it's better to insert your own token (watch README)
 vlzhr = vk_api.VkApi(token = vlzhr_token)
 
 vhse_token = u''
